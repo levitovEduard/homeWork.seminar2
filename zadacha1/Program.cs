47bba48bf5bb836cb60eb83cf6c22876890382fd
Eduard <levitov.eduard@gmail.com>
2022-12-04 14:56:46 +0200

    Поиск большего числа

---
+++ zadacha1/Program.cs
@@ -0,0 +1,8 @@
+﻿int num1= a; 
+int num2= b;
+    if (num1 > num2)
+            
+        Console.WriteLine("Max {0}, min {1}", num1, num2);
+            
+    else    
+        Console.WriteLine("Max {0}, min {1}", num2, num1);
